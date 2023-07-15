44564bf1e183e158884c88fc7abaf7666f2924f9
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-07-15 13:11:44 -0300

    feat: resolvido exercícios bônus

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ app.get('/ping', controllers.ping);
 app.post('/login', controllers.login);
 app.get('/users/me', middlewares.auth, controllers.me);
 app.get('/to-secret', middlewares.auth, middlewares.admin, controllers.topSecret);
+app.post('/signup', controllers.signup);
 
 app.use(middlewares.error);
 
--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,39 +1,21 @@
-const Joi = require('joi');
-const jwt = require('jsonwebtoken');
-
-const { JWT_SECRET } = process.env;
-
-const validateBody = (body) => Joi.object({
-  username: Joi.string().min(5).alphanum().required()
-  .messages({
-    'string.min': '"username" length must be 5 characters long',
-    'string.required': '"username" is required',
-  }),
-  password: Joi.string().min(5).required()
-  .messages({
-    'string.min': '"password" length must be 5 characters long',
-    'string.required': '"password" is required',
-  }),
-}).validate(body);
+const service = require('../services/User');
+const { validateCredentials } = require('./utils/validateCredentials');
 
 module.exports = async (req, res, next) => {
-  const { error } = validateBody(req.body);
+  const { error } = validateCredentials(req.body);
   if (error) return next(error);
 
-  if (req.body.username === 'admin' && req.body.password !== 's3nh4S3gur4???') {
-    const err = new Error('Invalid username or password');
-    err.statusCode = 401;
-    return next(err);
+  const { username, password } = req.body;
+
+  const { error: serviceError, token } = await service.login(username, password);
+
+  if (serviceError && serviceError.code === 'invalidCredentials') {
+    return next({ statusCode: 401, message: serviceError.message });
   }
 
-  const admin = req.body.username === 'admin' && req.body.password === 's3nh4S3gur4???';
-
-  const payload = {
-    username: req.body.username,
-    admin,
-  };
-
-  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+  if (serviceError) {
+    return next(serviceError);
+  }
 
   res.status(200).json({ token });
 };
